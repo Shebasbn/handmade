22028827fdf724155a5cf61b220d125f29c272b4
Sebas Bautista Navarro <s_bautistanavarro@fanshaweonline.ca>
2025-05-31 20:20:44 -0400

    setup handmade hero

---
+++ src/win32_handmade.cpp
@@ -0,0 +1,28 @@
+/* ================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Sebastian Bautista $
+   $Notice: $
+   ================================================================*/
+#include <windows.h>
+
+LRESULT CALLBACK
+WindowProc(HWND hwnd,
+	    UINT uMsg,
+	    WPARAM wParam,
+	    LPARAM lParam)
+{
+  
+}
+
+int WINAPI
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+	 PWSTR pCmdLine,
+	 int nCmdShow)
+{
+  WNDCLASS wc{};
+  
+  return 0;
+}
